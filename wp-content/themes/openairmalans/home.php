f62eea2af4ca4b9496696db6b9682bdb7d1e3d65
Marc Wieland <marcwieland95@gmail.com>
2017-05-28 11:46:04 +0200

    Add cpt impressions and include in twig object

--- wp-content/themes/openairmalans/home.php
+++ wp-content/themes/openairmalans/home.php
@@ -7,20 +7,28 @@ $args = array(
 	'post_type' => 'page',
 	'posts_per_page' => -1,
 	'order' => 'ASC',
-	'orderby' => 'menu_order',
+	'orderby' => 'menu_order'
 );
 
-
+// Grab artists
 $artists_args = array(
 	'post_type' => 'artists',
 	'posts_per_page' => -1,
 	'order' => 'ASC',
-	'orderby' => 'menu_order',
+	'orderby' => 'menu_order'
+);
+
+// Grab impressions
+$impressions_args = array(
+	'post_type' => 'impressions',
+	'posts_per_page' => -1,
+	'order' => 'ASC',
+	'orderby' => 'menu_order'
 );
 
 $context = Timber::get_context();
 $context['posts'] = new Timber\PostQuery( $args, 'Neocode\Theme\OnepagePost' );
-//$context['artists'] = new Timber\PostQuery( $artists_args );
 $context['artists'] = Timber::get_posts( $artists_args );
+$context['impressions'] = Timber::get_posts( $impressions_args );
 
 Timber::render('onepager.twig', $context);
