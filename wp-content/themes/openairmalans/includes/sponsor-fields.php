35d05326722530f3fbb90b7005e4056f63d8b488
Michael Wieland <michi.wieland@hotmail.com>
2017-06-25 18:31:03 +0200

    media sponsors added

--- wp-content/themes/openairmalans/includes/sponsor-fields.php
+++ wp-content/themes/openairmalans/includes/sponsor-fields.php
@@ -8,20 +8,28 @@ Container::make('post_meta', 'Sponsors')
   ->add_fields(array(
     Field::make('complex', 'crb_presenter')
       ->add_fields('Presenter', array(
-        Field::make("image", "crb_presenter_logo", "Logo")->set_required(true),
-        Field::make('text', 'crb_presenter_name', "Name des Sponsors")->set_required(true),
-        Field::make('text', 'crb_presenter_url', "Homepage des Sponsors")
+        Field::make("image", "crb_presenter_logo", "Logo des Presenter")->set_required(true),
+        Field::make('text', 'crb_presenter_name', "Name des Presenter")->set_required(true),
+        Field::make('text', 'crb_presenter_url', "Homepage des Presenter")
       )),
     Field::make('complex', 'crb_main_sponsors')
+        ->set_layout('tabbed-vertical')
         ->add_fields('Hauptsponsor', array(
-          Field::make("image", "crb_main_sponsors_logo", "Logo")->set_required(true),
-          Field::make('text', 'crb_main_sponsors_name', "Name des Sponsors")->set_required(true),
-          Field::make('text', 'crb_main_sponsors_url', "Homepage des Sponsors")
+          Field::make("image", "crb_main_sponsors_logo", "Logo des Hauptsponsors")->set_required(true),
+          Field::make('text', 'crb_main_sponsors_name', "Name des Hauptsponsors")->set_required(true),
+          Field::make('text', 'crb_main_sponsors_url', "Homepage des Hauptsponsors")
+        )),
+    Field::make('complex', 'crb_media_partner')
+        ->set_layout('tabbed-vertical')
+        ->add_fields('Hauptsponsor', array(
+          Field::make("image", "crb_media_partner_logo", "Logo des Medienpartner")->set_required(true),
+          Field::make('text', 'crb_media_partner_name', "Name des Medienpartner")->set_required(true),
+          Field::make('text', 'crb_media_partner_url', "Homepage des Medienpartner")
         )),
     Field::make('complex', 'crb_sponsors')
       ->set_layout('tabbed-vertical')
       ->add_fields('Sponsoren', array(
-        Field::make("image", "crb_sponsors_logo", "Logo")->set_required(true),
+        Field::make("image", "crb_sponsors_logo", "Logo des Sponsors")->set_required(true),
         Field::make('text', 'crb_sponsors_name', "Name des Sponsors")->set_required(true),
         Field::make('text', 'crb_sponsors_url', "Homepage des Sponsors")
       ))
