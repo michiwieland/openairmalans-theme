a074fdd06bc9bcaa7365200c9f69bf1f749c5a59
Marc Wieland <marcwieland95@gmail.com>
2017-07-01 12:43:11 +0200

    Fix impressum on single (fix #35)

--- wp-content/themes/openairmalans/single.php
+++ wp-content/themes/openairmalans/single.php
@@ -1,5 +1,12 @@
 <?php
 $context = Timber::get_context();
 
+// Grab impressum
+$impressum_object = get_page_by_title( 'Impressum' );
+$impressum_args = array(
+    'post_type'  => 'page',
+    'p' => $impressum_object->id
+);
+
 $context['posts'] = Timber::get_posts();
 Timber::render('single.twig', $context);
